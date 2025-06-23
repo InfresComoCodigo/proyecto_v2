4f693e1531eaf043854d5adec298f8d0cc71d3d4
ValXux <valenferxux@gmail.com>
2025-06-23 04:21:32 -0500

    :)

--- terraform/modules/security/firewall/main.tf
+++ terraform/modules/security/firewall/main.tf
@@ -1,50 +1,46 @@
-resource "aws_fms_policy" "egress_policy" {
-  name                  = "${var.env}-egress-policy"
-  resource_type         = "AWS::ElasticLoadBalancingV2::LoadBalancer"
+resource "aws_fms_policy" "example" {
+  name                  = "FMS-Policy-Example"
   exclude_resource_tags = false
+  remediation_enabled   = false
+  resource_type         = "AWS::ElasticLoadBalancingV2::LoadBalancer"
 
   security_service_policy_data {
     type = "WAFV2"
 
     managed_service_data = jsonencode({
       type = "WAFV2"
-      defaultAction = {
-        type = "ALLOW"
-      }
+      defaultAction = { type = "ALLOW" }
 
+      # 👉 Aquí SI hay una regla
       preProcessRuleGroups = [
         {
-          ruleGroupArn   = "arn:aws:wafv2:us-east-1:901539331960:regional/rulegroup/test_pre_rulegroup/86bd9b59-2503-4afe-8040-1e26fd43c880"
-          overrideAction = {
-            type = "NONE"
+          ruleGroupType  = "ManagedRuleGroup"
+          priority       = 1                     # 1-99
+          overrideAction = { type = "NONE" }
+          managedRuleGroupIdentifier = {
+            vendorName           = "AWS"
+            managedRuleGroupName = "AWSManagedRulesCommonRuleSet"
+            # versionEnabled     = true           # opcional
+            # version            = "Version_2.0"  # opcional
           }
-          ruleGroupType = "RuleGroup"
-          priority      = 1
+          ruleGroupArn = null                    # debe ser null cuando usas managedRuleGroupIdentifier
         }
       ]
 
-      postProcessRuleGroups = [
-        {
-          ruleGroupArn   = "arn:aws:wafv2:us-east-1:901539331960:regional/rulegroup/test_post_rulegroup/ba63bafc-8298-459c-8c21-a46e5a1d9764"
-          overrideAction = {
-            type = "NONE"
-          }
-          ruleGroupType = "RuleGroup"
-          priority      = 2
-        }
-      ]
-
-      overrideAction = {
-        type = "NONE"
-      }
-
-      loggingConfiguration = {
-        logDestinationConfigs = [
-          "arn:aws:s3:::aws-waf-logs-fms"
-        ]
-      }
+      postProcessRuleGroups = []                 # puede quedar vacío
+      overrideCustomerWebACLAssociation = false
     })
   }
+
+
+  tags = {
+    Name = "example-fms-policy"
+  }
+}
+
+resource "aws_wafregional_rule_group" "example" {
+  metric_name = "WAFRuleGroupExample"
+  name        = "WAF-Rule-Group-Example"
 }
 
 resource "aws_security_group" "ec2_sg" {
--- terraform/terraform.tfvars
+++
@@ -1,2 +0,0 @@
-project_name = "villa-alfredo-dev"
-aws_region   = "us-east-1"
